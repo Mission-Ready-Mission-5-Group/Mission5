e6a92b1104627d32d4d1bccefdb9a5a6ca543fc7
Manasa Jagarlapudi <manasajagarlapudi@Manasas-MacBook-Air.local>
2023-12-20 12:04:48 +1300

    added preline ui and a card just to test

--- frontend/src/routes/SingleListing.tsx
+++ frontend/src/routes/SingleListing.tsx
@@ -1,9 +1,15 @@
 import { useLoaderData } from "react-router-dom";
+import { GetInTouch } from "../features/single-listing/GetInTouch";
 
 const SingleListing = () => {
   const data = useLoaderData();
   return (
-    <h1 className="text-3xl font-bold text-pink-800">{JSON.stringify(data)}</h1>
+    <div>
+      <h1 className="text-3xl font-bold text-pink-800">
+        {JSON.stringify(data)}
+      </h1>
+      <GetInTouch />
+    </div>
   );
 };
 
