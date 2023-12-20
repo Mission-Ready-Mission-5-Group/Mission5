6087c6eb2ab928cd98df7f87bd8b645caa1f3245
s <s.looi2@outlook.com>
2023-12-20 18:48:48 +1300

    Validate update instead of blindly accepting data

--- mongo-cli/src/database/index.ts
+++ mongo-cli/src/database/index.ts
@@ -1,9 +1,10 @@
 import mongoose from "mongoose";
 
-export const initDB = () =>
-  mongoose.connect("mongodb://listing-db:27017/missionfive");
+export const initDB = () => {
+  mongoose.connect("mongodb://0.0.0.0:27018/missionfive");
   mongoose.plugin(schema => {
     schema.pre('findOneAndUpdate', function () { this.setOptions({ runValidators: true }) });
     schema.pre('updateMany', function () { this.setOptions({ runValidators: true }) });
     schema.pre('updateOne', function () { this.setOptions({ runValidators: true }) });
   });
+}
