cbd01f31502569004d4906607c7338925e957779
s <s.looi2@outlook.com>
2023-12-21 04:40:14 +1300

    Add filtering

--- frontend/src/features/Sam/Hero.tsx
+++ frontend/src/features/Sam/Hero.tsx
@@ -7,22 +7,37 @@ import { RiArrowDropUpLine } from "react-icons/ri";
 				<div className="absolute bg-[#333333] inset-0 flex justify-center items-center">
 */
 type Places = "Parnell" | "NewMarket"
-
+type Filter = 'Gyms' | 'Parks' | 'Supermarkets' | 'Cinemas' | 'Swimming Pools' | "Pet Friendly" | "Has Elevators" | "Furnished"
+// city, petFriendly, hasElevators, furnished, gym, park, supermarket, cinema, swimmingPool
+/* 'gym', 'park', 'supermarket', 'cinema', 'swimmingPool' */
 const Hero = () => {
-	const [suburb, setSuburb] = useState<Places>("Parnell")
+	// Place
+	const [place, setPlace] = useState<Places>("Parnell")
 	const [placesIsOpened, setPlacesIsOpened] = useState<boolean>(false)
+	const placesButtonRef = useRef<HTMLDivElement>()
 
-	const placesButton = useRef<HTMLDivElement>()
+	// Filters
+	const [filters, setFilters] = useState<Filter[]>([])
+	const [filtersIsOpen, setFiltersIsOpen] = useState<boolean>(false)
+	const filtersButtonRef = useRef<HTMLDivElement>()
 
+	// Methods
+	const addFilter = (newFilter: Filter) => {
+		setFilters(filters => {
+			const newFilters = filters.includes(newFilter) ? filters : [newFilter, ...filters]
+			console.log("newFilters\t", newFilters)
+			return newFilters
+		})
+	}
+
+
+	// Hooks
 	useEffect(() => {
 		const clickHandler = (e: MouseEvent) => {
-			console.log(placesButton.current)
-			if (placesButton.current && placesButton.current.contains(e.target as Node)) {
-				console.log("CONTAINS")
-			} else {
-				console.log("NOT HAVE!")
-				setPlacesIsOpened(false)
-			}
+			console.log(placesButtonRef.current)
+			if (!(placesButtonRef.current && placesButtonRef.current.contains(e.target as Node))) setPlacesIsOpened(false)
+			if (!(filtersButtonRef.current && filtersButtonRef.current.contains(e.target as Node))) setFiltersIsOpen(false)
+
 		}
 
 		document.addEventListener("mousedown", clickHandler)
@@ -32,25 +47,59 @@ const Hero = () => {
 	return (
 		<>
 			<div className='relative '>
+				{/* Background */}
 				<img
 					src={heroimg}
 					alt=""
 					className="w-full object-cover  h-[500px]"
 				/>
-				<div className="absolute bg-black bg-opacity-10 inset-0 flex justify-center items-center">
-					<div className="w-3/5 text-center bg-[#000] bg-opacity-40 flex p-6 rounded-2xl">
-						<div className="dropdown" ref={placesButton}>
-							<div tabIndex={0} role="button" className="btn m-1  w-[200px] flex justify-between" onClick={() => setPlacesIsOpened(true)}>
-								<div>{suburb}</div>
-								<div>{placesIsOpened ? <RiArrowDropUpLine size={25} /> : <RiArrowDropDownLine size={25} />}</div>
+
+				<div className="absolute bg-black bg-opacity-10 inset-0 flex justify-center items-center note-covers-image-to-allow-centering-of-insides">
+					<div className="text-center bg-[#000] bg-opacity-40 flex p-6 rounded-2xl flex-col note-this-is-the-container-for-all-the-search-related-elements">{/* w-3/5  */}
+
+						<div className='flex search-layer'>
+
+							{/* Location Button */}
+							<div className="dropdown" ref={placesButtonRef}>
+								<div tabIndex={0} role="button" className="btn m-1  w-[200px] flex justify-between" onClick={() => setPlacesIsOpened(true)}>
+									<div>{place}</div>
+									<div>{placesIsOpened ? <RiArrowDropUpLine size={25} /> : <RiArrowDropDownLine size={25} />}</div>
+								</div>
+								<ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
+									<li><a onClick={() => setPlace("Parnell")}>Parnell</a></li>
+									<li><a onClick={() => setPlace("NewMarket")}>NewMarket</a></li>
+								</ul>
 							</div>
-							<ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
-								<li><a onClick={() => setSuburb("Parnell")}>Parnell</a></li>
-								<li><a onClick={() => setSuburb("NewMarket")}>NewMarket</a></li>
-							</ul>
+
+							{/* Filter Button */}
+							<div className="dropdown" ref={filtersButtonRef}>
+								<div tabIndex={0} role="button" className="btn m-1  w-[200px] flex justify-between" >
+									<div>Select Filter</div>
+									<div>{filtersIsOpen ? <RiArrowDropUpLine size={25} /> : <RiArrowDropDownLine size={25} />}</div>
+								</div>
+								<ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
+									<li><a onClick={() => addFilter("Gyms")}>Gyms</a></li>
+									<li><a onClick={() => addFilter("Parks")}>Parks</a></li>
+									<li><a onClick={() => addFilter("Supermarkets")}>Supermarkets</a></li>
+									<li><a onClick={() => addFilter("Cinemas")}>Cinemas</a></li>
+									<li><a onClick={() => addFilter("Swimming Pools")}>Swimming Pools</a></li>
+									<li><a onClick={() => addFilter("Pet Friendly")}>Pet Friendly</a></li>
+									<li><a onClick={() => addFilter("Has Elevators")}>Has Elevators</a></li>
+									<li><a onClick={() => addFilter("Furnished")}>Furnished</a></li>
+								</ul>
+							</div>
+						</div>
+						<div className='flex flex-wrap'> {/*  bg-[#500] */}
+							{
+								filters.map(filter => (
+									<span className="badge m-1" key={filter}>{filter}</span>
+								))
+							}
 						</div>
 					</div>
 				</div>
+
+
 			</div>
 		</>
 	);
