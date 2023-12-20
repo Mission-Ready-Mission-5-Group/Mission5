077894ffdf451c7f69370dfe2ac05aae7e79450b
s <s.looi2@outlook.com>
2023-12-20 23:52:51 +1300

    testing variable bgs

--- frontend/src/features/Sam/Hero.tsx
+++ frontend/src/features/Sam/Hero.tsx
@@ -2,17 +2,29 @@ import React from 'react'
 import heroimg from '../../assets/banner.jpg'
 import "./Hero.css"
 
+
+/* 
+				<div className="absolute bg-[#333333] inset-0 flex justify-center items-center">
+*/
 const Hero = () => {
-	
+	const test = true
+	const bg1 = true && '#333333';
+	const bg2 = test && '#FFFF';
 	return (
 		<>
-			<div className='relative'>
+			<div className='relative '>
 				<img
-				src={heroimg}
-				alt=""
-				className="w-full object-cover  h-[500px]"
+					src={heroimg}
+					alt=""
+					className="w-full object-cover  h-[500px]"
 				/>
-				<div className="black-background"> hiaskdjashd kjashd ksah dksahkashdkjh</div>
+				<div className={`absolute bg-${bg1} inset-0 flex justify-center items-center`}>
+					<div className={`w-96 text-center bg-${"black"}`}>
+						<p>Your content here</p>
+						<p>Your content here</p>
+						<p>Your content here</p>
+					</div>
+				</div>
 			</div>
 		</>
 	);
