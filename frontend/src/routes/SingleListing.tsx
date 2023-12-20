6f53cc05421fd34837e163c14cbff53cb324daa9
Manasa Jagarlapudi <manasajagarlapudi@Manasas-MacBook-Air.local>
2023-12-20 15:31:08 +1300

    created single-listing page's layout

--- frontend/src/routes/SingleListing.tsx
+++ frontend/src/routes/SingleListing.tsx
@@ -1,18 +1,31 @@
 import { useLoaderData } from "react-router-dom";
 import { GetInTouch } from "../features/single-listing/GetInTouch";
 import { Description } from "../features/single-listing/Description";
+import { MoveInCalc } from "../features/single-listing/MoveInCalc";
+import { Calendar } from "../features/single-listing/Calendar";
+import { PropertyInfo } from "../features/single-listing/PropertyInfo";
+import { Maps } from "../features/single-listing/Maps";
+import { PropertyPicsCollage } from "../features/single-listing/PropertyPicsCollage";
 
 const SingleListing = () => {
   const data = useLoaderData();
   return (
     <div>
-      <h1 className="text-3xl font-bold text-pink-800">
+      {/* <h1 className="text-3xl font-bold text-pink-800">
         {JSON.stringify(data)}
-      </h1>
-      <div className="flex gap-2">
-        <GetInTouch />
-        <Description />
+      </h1> */}
+
+      <div className="flex">
+        <PropertyPicsCollage />
+        <div>
+          <Calendar />
+          <MoveInCalc />
+          <GetInTouch />
+        </div>
       </div>
+      <Description />
+      <PropertyInfo />
+      <Maps />
     </div>
   );
 };
