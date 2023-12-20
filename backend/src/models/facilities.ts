e3e0be142b3ac3bea18b4b3074350ae4864cfe33
s <s.looi2@outlook.com>
2023-12-20 16:58:55 +1300

    Add facility schema and data

---
+++ backend/src/models/facilities.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+// Maps Schema in Mongoose
+const SchemaFacility = new mongoose.Schema({
+	facility: {
+		type: String,
+		enum: ['gym', 'park', 'supermarket', 'cinema', 'swimmingPool'],
+		required: true
+	},
+	longitude:{
+		type: Number,
+		required: true
+	},
+	lattitude:{
+		type: Number,
+		required: true
+	}
+}, { timestamps: true, strict: "throw" })
+
+
+export const ModelFacilities = mongoose.model("facilities",SchemaFacility)
--- mongo-cli/src/commands/seedCmd.ts
+++ mongo-cli/src/commands/seedCmd.ts
@@ -1,8 +1,10 @@
+import { facilities } from "../factilitesStock";
 import { listingsArray } from "../listingsStock";
+import { facility } from "../models/facility";
 import { listing } from "../models/listing";
 
 export const seedCmd = async () => {
   console.log("Seeding listings");
-  const result = await listing.insertMany(listingsArray);
-  console.log(result);
+  console.log(await listing.insertMany(listingsArray));
+  console.log(await facility.insertMany(facilities));
 };
