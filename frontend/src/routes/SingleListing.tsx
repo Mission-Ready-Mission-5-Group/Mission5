06a016b928a1b1f1f27b0574561a946222047b9b
Manasa Jagarlapudi <manasajagarlapudi@Manasas-MacBook-Air.local>
2023-12-21 01:45:52 +1300

    added map

--- frontend/src/routes/SingleListing.tsx
+++ frontend/src/routes/SingleListing.tsx
@@ -4,13 +4,13 @@ import { Description } from "../features/single-listing/Description";
 import { MoveInCalc } from "../features/single-listing/MoveInCalc";
 import { Calendar } from "../features/single-listing/Calendar";
 import { PropertyInfo } from "../features/single-listing/PropertyInfo";
-import { Maps } from "../features/single-listing/Maps";
+import Maps from "../features/single-listing/Maps";
 import { PropertyPicsCollage } from "../features/single-listing/PropertyPicsCollage";
 
 const SingleListing = () => {
   const data = useLoaderData();
   return (
-    <div className="p-16 space-y-8">
+    <div className="px-16 pb-16 space-y-8">
       {/* <h1 className="text-3xl font-bold text-pink-800">
         {JSON.stringify(data)}
       </h1> */}
